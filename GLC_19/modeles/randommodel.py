a09b921671338611b31d8b2acfee863db868b55f
4bach <yanniskarmim@hotmail.fr>
2019-03-24 20:04:08 +0100

    modele JUL

--- GLC_19/modeles/randommodel.py
+++ GLC_19/modeles/randommodel.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-from vector_model import Classifier
+from classifier import Classifier
 
 class RandomModel(Classifier):
 
@@ -9,24 +9,48 @@ class RandomModel(Classifier):
        space
     """
     def __init__(self):
-        """Does nothing
-        """
+
         pass
 
     def fit(self, dataset):
-        """Does nothing
-        """
-        self.trainset = dataset
-    def predict(self, dataset, ranking_size=100):
+
+        self.train_set = dataset
+        self.all_labels = pd.Series(dataset.labels.unique())
+
+    def predict(self, dataset, ranking_size=30):
 
         predictions = []
-        all_labels = self.trainset.labels
-
         for j in range(len(dataset)):
-            toplabels = np.random.choice(all_labels, size=ranking_size)
-            predictions.append(toplabels)
+            y_predicted = list (np.random.choice(self.all_labels.values, size=ranking_size))
+            predictions.append(y_predicted)
 
         return predictions
 
 if __name__ == '__main__':
-    pass
+    from glcdataset import GLCDataset
+
+    print("Random model tested on train set\n")
+    df = pd.read_csv('example_occurrences.csv', sep=';', header='infer', quotechar='"', low_memory=True)
+    df = df.dropna(axis=0, how='all')
+    df = df.astype({'glc19SpId': 'int64'})
+    glc_dataset = GLCDataset(df[['Longitude','Latitude']], df['glc19SpId'],
+                             scnames=df[['glc19SpId','scName']],patches_dir='example_envtensors/0')
+
+    randommodel = RandomModel()
+
+    randommodel.fit(glc_dataset)
+    predictions = randommodel.predict(glc_dataset)
+    scnames = randommodel.train_set.scnames
+    for idx in range(4):
+
+        y_predicted = predictions[idx]
+        print("Occurrence:", randommodel.train_set.data.iloc[idx].values)
+        print("Observed specie:", scnames.iloc[idx]['scName'])
+        print("Predicted species, ranked:")
+
+        print([scnames[scnames.glc19SpId == y]['scName'].iloc[0] for y in y_predicted[:10]])
+        print('\n')
+
+    print("Top30 score:",randommodel.top30_score(glc_dataset))
+    print("MRR score:", randommodel.mrr_score(glc_dataset))
+    print("Cross validation score:", randommodel.cross_validation(glc_dataset, 4, shuffle=False, evaluation_metric='mrr'))
